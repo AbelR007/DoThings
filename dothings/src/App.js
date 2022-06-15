ad56d1d58ad06fcfe101273bb7891bf978dd2487
AbelR007 <abelroi007@gmail.com>
2022-06-15 16:21:31 +0530

    App v1.3 - Setup React Router

--- dothings/src/App.js
+++ dothings/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-
+      <Router>
         <div className="container">
           <h2>DoThings Web App</h2>
         </div>
-
+      </Router>
     );
   }
 }
